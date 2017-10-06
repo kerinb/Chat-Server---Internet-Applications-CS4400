9c5e77d22265a9437be353c4b90eacbfc50755af
Breandan <kerinb@tcd.ie>
2017-10-06 12:51:48 +0100

    adding in previously unversioned files, and extra stuff in main files

--- src/com/company/ClientThread.java
+++ src/com/company/ClientThread.java
@@ -1,4 +1,15 @@
 package com.company;
 
-public class ClientThread {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientThread extends Thread {
+    private ObjectOutputStream sOutput; // write on the socket
+    private ObjectInputStream sInput; // read from the socket
+    private Socket socket; // the socket in question
+
+    private String server, username;
+    private int portNumber;
+
 }
