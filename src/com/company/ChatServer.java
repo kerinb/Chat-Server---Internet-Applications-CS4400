5025981564519ec983472c0d59d5e130427eccec
Breandan <kerinb@tcd.ie>
2017-10-28 16:02:00 +0100

    edits made to chatServer File

--- src/com/company/ChatServer.java
+++ src/com/company/ChatServer.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.security.ntlm.Client;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -7,6 +9,8 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.ConcurrentSkipListSet;
@@ -41,24 +45,29 @@ public class ChatServer {
 
 	public static void main(String[] args) {
 		try {
-			initialiseServer();
+			initialiseServer(args[0]);
 			try {
 				while (!terminateServer.equals(Boolean.TRUE)) {
 					handleClientConnection();
 				}
 			} catch (Exception e) {
-				System.out.println(String.format("Error encountered with client connection: %s", e.getMessage()));
+
+                String ErrorMessage = "ERROR: occurred with client connection " + e.getMessage() + " \n OCCURRED: "
+                        + ErrorHandler.getTodaysDate();
+                System.out.println(ErrorMessage);
 			}
 		} catch (Exception e){
-			// TODO some sort of logging here 
-			System.out.println(String.format("Error encountered with server initialisation: %s", e.getMessage()));
+		    String ErrorMessage = "ERROR: encountered with server initialisation: " + e.getMessage() + " \n OCCURRED: "
+                    + ErrorHandler.getTodaysDate();
+			System.out.println(ErrorMessage);
 		}
 		finally {
 			try {
 				killServer(false);
 			} catch (IOException e) {
-				// TODO
-				e.printStackTrace();
+                String ErrorMessage = "ERROR: encountered with killing server : " + e.getMessage() + " \n OCCURRED: "
+                        + ErrorHandler.getTodaysDate();
+                System.out.println(ErrorMessage);
 			}
 		}
 	}
@@ -80,9 +89,13 @@ public class ChatServer {
 	private static void handleClientConnection() throws IOException {
 		// accept connection and identify request type
 		Socket clientSocket = serverSocket.accept();
-		RequestType requestType = parseRequestType(clientSocket);
-		handleClientRequest(requestType);
-		updateServerRecordsIfRequired(requestType, clientSocket);
+		RequestType requestType = request(clientSocket);
+        ClientNode clientNode = getClientInfoFromMessage(clientSocket, requestType);
+        List<String> message = getMessageFromClient(clientSocket);
+        ClientThread newConnectedClientThread = new ClientThread(clientNode, requestType, message);
+
+        newConnectedClientThread.run();
+
 	}
 
 	private static void updateServerRecordsIfRequired(RequestType requestType, Socket clientSocket) throws IOException {
@@ -124,7 +137,7 @@ public class ChatServer {
     }
 
     private static void removeClientFromServer(ClientNode clientNode, ChatRoom chatRoom){
-        
+
     }
 
 	private static void removeClientRecord(Socket clientSocket) {
@@ -155,23 +168,83 @@ public class ChatServer {
 		}
 	}
 
-	private static void handleClientRequest(RequestType parseRequestType) {
-		// TODO @Breand�n
-		// Based on the type, we should either create a new thread or not
-		// Depending on the type of request, may need different threads
-	}
+	private static synchronized void handleClientRequest(RequestType parseRequestType) throws IOException {
+		Socket clientSocket = serverSocket.accept();
+        String message = "New connection received from %s..." + clientSocket.getInetAddress().toString();
+        System.out.println(message);
 
-	private static RequestType parseRequestType(Socket clientSocket) throws IOException {
+        RequestType requestType = request(clientSocket);
+        ClientNode clientNode = getClientInfoFromMessage(clientSocket, requestType);
+        List<String> messages = getMessageFromClient(clientSocket);
+        ClientThread newClientThread = new ClientThread(clientNode, requestType, messages);
+    }
+
+    public static RequestType request(Socket clientSocket) throws IOException {
+        String request = parseRequestType(clientSocket);
+        try{
+            return RequestType.valueOf(request);
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public static ClientNode getClientInfoFromMessage(Socket clientSocket, RequestType requestType) throws IOException {
+        List<String> entireMessageFromClient = getMessageFromClient(clientSocket);
+        switch (requestType){
+            case JoinChatroom:
+                return new ClientNode(entireMessageFromClient.get(3).split(CLIENT_NAME_IDENTIFIER, 0)[1],
+                        entireMessageFromClient.get(0).split(JOIN_CHATROOM_IDENTIFIER, 0)[1], clientId.getAndIncrement(),
+                        clientSocket);
+            case Chat:
+                return new ClientNode(entireMessageFromClient.get(2).split(CLIENT_NAME_IDENTIFIER, 0)[1],
+                        entireMessageFromClient.get(0).split(CHAT_IDENTIFIER, 0)[1],
+                        Integer.parseInt(entireMessageFromClient.get(1).split(JOIN_ID_IDENTIFIER, 0)[1]), clientSocket);
+            case LeaveChatroom:
+                return new ClientNode(entireMessageFromClient.get(2).split(CLIENT_NAME_IDENTIFIER, 0)[1],
+                        entireMessageFromClient.get(0).split(LEAVE_CHATROOM_IDENTIFIER, 0)[1],
+                        Integer.parseInt(entireMessageFromClient.get(1).split(JOIN_ID_IDENTIFIER, 0)[1]), clientSocket);
+            case Disconnect:
+                return new ClientNode(entireMessageFromClient.get(2).split(CLIENT_NAME_IDENTIFIER, 0)[1], null,
+                        UNDEFINED_JOIN_ID, clientSocket);
+            case HelloText:
+                return new ClientNode(null, null, UNDEFINED_JOIN_ID, clientSocket);
+            case KillService:
+                return new ClientNode( null, null, UNDEFINED_JOIN_ID, clientSocket);
+            default:
+                return null;
+        }
+    }
+
+    private static List<String> getMessageFromClient(Socket socket) throws IOException {
+        BufferedReader messageFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        List<String> linesOfTextFromClient = new LinkedList<String>();
+        String lineOfText = messageFromClient.readLine();
+        while (lineOfText!= null){
+            linesOfTextFromClient.add(lineOfText);
+            lineOfText = messageFromClient.readLine();
+        }
+        return linesOfTextFromClient;
+    }
+
+	private static String parseRequestType(Socket clientSocket) throws IOException {
 		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		String clientSentence = inFromClient.readLine();
 		String[] request = clientSentence.split(":", 1);
-		return RequestType.valueOf(request[0]);
+		return request[0];
 	}
 
-	private static void initialiseServer() throws IOException {
-		terminateServer = new AtomicBoolean(false);
-		serverSocket.bind(SERVER_ADDRESS, SERVER_PORT);
-		// TODO any other initialisation		
+	private static void initialiseServer(String portNumber) throws IOException {
+		serverPort = Integer.parseInt(portNumber);
+		serverSocket = new ServerSocket(serverPort);
+		initialiseServerValues();
+		String message = "Serveris listening on port number: %s " + portNumber;
+		System.out.println(message);
+	}
+
+	private static void initialiseServerValues(){
+		ListOfActiveChatRooms = new ConcurrentSkipListMap<ChatRoom, ConcurrentSkipListSet<ClientNode>>();
+		terminateServer = new AtomicBoolean(Boolean.FALSE);
+		clientId = new AtomicInteger(0);
 	}
 
 	public static int getServerPortNumber() {
